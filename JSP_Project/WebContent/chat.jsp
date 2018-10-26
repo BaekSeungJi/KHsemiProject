57dfcfab68f014443bde9ea9298406b74bdb35a2
fnvlektmf <fnvlektmf@naver.com>
2018-10-26 19:59:26 +0900

    ㅇ

---
+++ JSP_Project/WebContent/chat.jsp
@@ -0,0 +1,201 @@
+<%@page import="dto.MemberDto"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    
+    
+<%
+Object ologin = session.getAttribute("login");
+MemberDto mem = null;
+if(ologin != null){
+	mem = (MemberDto)ologin;
+}
+String userID = mem.getId();
+String toID = request.getParameter("toID");
+System.out.println("userID = " + userID);
+System.out.println("toID = " + toID);
+%>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="css/bootstrapCss/bootstrap.css">
+	<link rel="stylesheet" href="css/bootstrapCss/custom.css">
+	<title>Ajax 실시간 회원제채팅 서비스</title>
+	<script src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
+	<script src="js/bootstrapJs/bootstrap.js"></script>
+
+</head>
+	<script type="text/javascript">
+		function autoClosingAlert(selector, delay) {
+			var alert = $(selector).alert();
+			alert.show();
+			window.setTimeOut(function () { alert.hide() }, delay);
+		}
+		function submitFunction() {
+			var fromID = "<%=userID%>";
+			var toID = "<%=toID%>";
+			var chatContent = $("#chatContent").val();
+			$.ajax({
+				type : "POST",
+				url : "ChatControl",
+				data : {
+					"command" : "addChat",
+					"fromID" : encodeURIComponent(fromID),
+					"toID" : encodeURIComponent(toID),
+					"chatContent" : encodeURIComponent(chatContent)
+				},
+				success : function (result) {
+					if(result == 1){	// 입력 성공
+						autoClosingAlert('#successMessage', 2000);	// successMessage를 2초동안 보여줌.
+					}else if(result == -1){	// 입력 실패
+						autoClosingAlert('#dangerMessage', 2000);
+					}else{	// 데이터베이스 오류 발생
+						autoClosingAlert('#warningMessage', 2000);
+					}
+				}
+				
+			});
+			$("#chatContent").val("");	// 입력창을 비워줌
+			
+		}
+		
+		var lastID = 0;
+		function chatListFunction( type ) {
+			var fromID = '<%=userID%>';
+			var toID = '<%=toID%>';
+			$.ajax({
+				type : "GET",
+				url : "ChatControl",
+				data : {
+					"command" : "chatList",
+					"fromID" : encodeURIComponent(fromID),
+					"toID" : encodeURIComponent(toID),
+					"listType" : type
+				},
+				success : function (data) {
+					if(data == "") return;
+					var parsed = JSON.parse(data);
+					var result = parsed.result;
+					for (var i = 0; i < result.length; i++) {
+						if(result[i][0].value == fromID){
+							result[i][0].value = "나";
+						}
+						addChat(result[i][0].value, result[i][2].value, result[i][3].value);
+					}
+					lastID = Number(parsed.last);	// 가장마지막으로 전달받은 seq
+				}
+			});
+			
+		}
+		
+		// 채팅창에 채팅내역 바로바로 출력
+		function addChat(chatName, chatContent, chatTime) {
+			$("#chatList").append('<div class="row">' + 
+					'<div class="col-lg-12">' +
+					'<div class="media">' +
+					'<a class="pull-left" href="#">' + 
+					'<img class="media-object img-circle" style="width : 30px; height : 30px;" src="css/images/icon.png" alt="">' +
+					'<a>' +
+					'<div class="media-body">' +
+					'<h4 class="media-heading">' +
+					chatName +
+					'<span class="small pull-right">' +
+					chatTime +
+					'</span>' +
+					'</h4>' +
+					'<p>' +
+					chatContent +
+					'</p>' +
+					'</div>' +
+					'</div>' +
+					'</div>' +
+					'</div>' +
+					'<hr>'
+			);
+			$("#chatList").scrollTop($("#chatList")[0].scrollHeight);
+			
+		}
+		
+		// 3초에 한번씩 채팅창 목록 자동업뎃
+		function getInfiniteChat() {
+			setInterval(function () {
+				chatListFunction(lastID);
+			}, 3000);
+		}
+	</script>
+<body>
+
+
+<nav class="navbar navbar-default">
+	<div class="navbar-header">
+			<a class="navbar-brand" href="#">실시간 회원제 채팅 서비스</a>
+	</div>
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			<ul class="nav navbar-nav">
+				<li><a href="index.jsp">Home</a>
+				<li><a href="chatBox.jsp">메시지함</a>
+				<li><a href="mypage.jsp">마이페이지</a>
+				<li><a href="logout.jsp">로그아웃</a>
+			</ul>
+			
+		</div>
+</nav>
+
+<div class="container">
+	<div class="container bootstrap snippet">
+		<div class="row">
+			<div class="col-xs-12">
+				<div class="portlet portlet-default">
+					<div class="portlet-heading">
+						<div class="portlet-title">
+							<h4><i class="fa fa-circle text-green"></i>실시간 채팅방</h4>
+						</div>
+						<div class="clearfix"></div>
+					</div>
+					<div id="chat" class="panel-collapse collapse in">
+						<div id="chatList" class="portlet-body chat-widget" style="overflow-y: auto; width: auto; height: 600px;">
+						</div>
+						<div class="portlet-footer">
+							<!-- <div class="row">
+								<div class="form-group col-xs-4">
+									<input style="height: 40px;" type="text" id="chatName" class="form-control" placeholder="이름" maxlength="8">
+								</div>
+							</div> -->
+							<div class="row" style="height: 90px;">
+								<div class="form-group col-xs-10">
+									<textarea style="height: 80px;" id="chatContent" class="form-control" placeholder="메시지를 입력하세요" maxlength="100"></textarea>
+								</div>
+								<div class="form-group col-xs-2">
+									<button type="button" class="btn btn-default pull-right" onclick="submitFunction();">전송</button>
+									<div class="clearfix"></div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="alert alert-success" id="successMessage" style="display: none;">
+	<strong>메시지 전송에 성공했습니다.</strong>
+</div>
+<div class="alert alert-danger" id="dangerMessage" style="display: none;">
+	<strong>이름과 내용을 모두 입력해주세요.</strong>
+</div>
+<div class="alert alert-warning" id="warningMessage" style="display: none;">
+	<strong>데이터베이스 오류가 발생했습니다.</strong>
+</div>
+
+<script type="text/javascript">
+	$(document).ready(function () {
+		chatListFunction('0');
+		getInfiniteChat();	// 3초간격으로 계속 가져올수 있도록
+	});
+</script>
+	
+
+</body>
+</html>
---
+++ JSP_Project/src/control/MemberControl.java
@@ -0,0 +1,380 @@
+package control;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.jsp.PageContext;
+
+import dto.HotelDto;
+import dto.MemberDto;
+import dto.MonthlysalesDto;
+import dto.ReserveDto;
+import dto.ReserveTableDto;
+import dto.ReviewDto;
+import dto.scoreDto;
+import dto.util;
+import model.hotel.HotelService;
+import model.member.MemberService;
+import model.reserve.ReserveService;
+import model.review.ReviewService;
+
+import javax.servlet.RequestDispatcher;
+
+public class MemberControl extends HttpServlet {
+
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+		try {
+			doProcess(req, resp);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+		try {
+			doProcess(req, resp);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+
+	public void doProcess(HttpServletRequest req, HttpServletResponse resp) throws Exception, ServletException {
+
+		req.setCharacterEncoding("utf-8");
+		resp.setContentType("text/html; charset=UTF-8");
+
+		String command = req.getParameter("command");
+
+		if(command.equals("Signup")) {						//회원가입
+			resp.sendRedirect("Signup.jsp");
+		}		
+		else if (command.equals("회원가입")) {					//회원가입Af
+			String id = req.getParameter("id");
+			String pwd = req.getParameter("pwd");
+			String name = req.getParameter("name");
+			String email = req.getParameter("email");
+			String phone = req.getParameter("phone");
+			int blacklist = 0;
+			int auth = 3;
+					
+			MemberDto dto = new MemberDto(id, pwd, name, email, phone, blacklist, auth);
+			req.setAttribute("dto", dto);
+			dispatch("SignupAf.jsp", req, resp);
+
+		}
+		else if(command.equals("Login")) {					//로그인
+			
+			String id = req.getParameter("id");
+			String pwd = req.getParameter("pwd");
+		/*	String name = req.getParameter("name");
+			String email = req.getParameter("email");
+			String phone = req.getParameter("phone");
+			int blacklist = 0;
+			int auth = 3;*/
+			/*	int blacklist = ((Integer)req.getAttribute("blacklist")).intValue();
+			int auth = ((Integer)req.getAttribute("auth")).intValue();*/
+		
+			MemberDto dto = new MemberDto(id, pwd, null, null, null, 0, 0);
+			req.setAttribute("dto", dto);
+			
+			/*
+			req.setAttribute("id", id);
+			req.setAttribute("pwd", pwd);
+			req.setAttribute("name", name);
+			req.setAttribute("email", email);
+			req.setAttribute("phone", phone);
+			req.setAttribute("blacklist", 0);
+			req.setAttribute("auth", 3);
+			*/
+			dispatch("login.jsp", req, resp);	
+		
+		}else if (command.equals("아이디찾기")) {				//아이디찾기
+			resp.sendRedirect("Suchidview.jsp");
+			
+		}else if (command.equals("비밀번호찾기")) {				//비밀번호찾기
+			resp.sendRedirect("Suchpwdview.jsp");
+			
+		}else if (command.equals("아이디확인")) {				//아이디 확인
+			String email = req.getParameter("email");
+			req.setAttribute("email", email);
+			dispatch("SuchidAf.jsp", req, resp);
+		
+		}else if (command.equals("비밀번호확인")){				//비밀번호 확인
+			String id = req.getParameter("id");
+			String phone = req.getParameter("phone");
+			req.setAttribute("id", id);
+			req.setAttribute("phone", phone);
+			dispatch("SuchpwdAf.jsp", req, resp);
+			
+		}else if(command.equals("ad_admin")) {
+			
+			//로그인정보가져오기
+			MemberDto dto = (MemberDto)req.getSession().getAttribute("login");
+			String id = dto.getId();
+			
+	
+			// 메인관리자면 바로 게시판으로 ㄱ
+						String link = null;
+						if(dto.getAuth()==1) {
+							link = "PdsControl?command=ad_noticeGo";
+						}else {
+							link = "adminMain.jsp";
+						
+						
+						//서비스 생성
+						MemberService service = MemberService.getInstance();
+						ReviewService Rservice = ReviewService.getInstance();
+						ReserveService ser = ReserveService.getInstance();
+						HotelService Hser = HotelService.getInstance();
+						
+						// 호텔이름가져와서 세션저장
+						String hotelname= service.getHotelname(id);
+						req.getSession().setAttribute("hotelname", hotelname);
+						HotelDto hoteldto = Hser.getHoteldetail(hotelname);
+						
+						System.out.println("호텔 이미지 경로 컨트롤러 :"+hoteldto.getImage());
+						
+						// 차트 정보
+						int price = Integer.parseInt(Hser.getPrice(hotelname));
+						List<ReviewDto> Rlist = Rservice.ad_getReview(hotelname);
+						List<ReserveDto> Rdto = ser.getReserve(hotelname);
+						scoreDto sdto = util.getScore(Rlist);
+						MonthlysalesDto Mdto =  util.getsales(Rdto, price);
+						
+					
+						req.setAttribute("hoteldto", hoteldto);
+						req.setAttribute("Mdto", Mdto);
+						req.setAttribute("sdto", sdto);
+						}
+						
+						dispatch(link , req, resp);
+					
+					}
+		
+		// 해당 호텔 예약한 회원들 보기 
+		else if(command.equals("memberGo")) {
+			
+			String findWord = req.getParameter("txt");
+			String choice = req.getParameter("sel");
+
+			System.out.println("findWord = " + findWord);
+
+			System.out.println("choice = " + choice);
+
+			if(choice == null || choice.equals("")){
+				choice = "";	
+			}
+
+			if(findWord == null){
+				findWord = "";
+			}
+			
+			String id = req.getParameter("id");
+			System.out.print(id);
+			MemberService service = MemberService.getInstance();
+			
+			String hotelname= service.getHotelname(id);
+			System.out.print(hotelname);
+			
+			List<ReserveTableDto> list = service.ad_GetHotelmember(hotelname, findWord, choice);
+			
+			req.setAttribute("list", list);
+			
+			dispatch("ad_member.jsp", req, resp);
+			
+		
+		}
+		
+		// 해당 회원 예약,리뷰 리스트 보기 
+		else if(command.equals("ad_member_detail")) {
+			
+		
+			String id = req.getParameter("id");
+			
+			System.out.println("mem컨트롤 id 테스트"+id);
+			MemberService service = MemberService.getInstance();
+			
+			List<ReserveDto> reserveList = service.ad_reserveList(id); 
+			
+			List<ReviewDto> reviewList = service.ad_reviewList(id);
+			
+			req.setAttribute("reviewList", reviewList);
+			
+			req.setAttribute("reserveList", reserveList);
+			
+			dispatch("ad_member_detail.jsp", req, resp);
+		}
+		
+		
+		else if(command.equals("ad_memberUpdate")) {
+			System.out.println("들어옴~~~~~");
+			String id = req.getParameter("id");
+			String pw = req.getParameter("pw");
+			String name = req.getParameter("name");
+			String email = req.getParameter("email");
+			String phone = req.getParameter("phone");
+			
+			MemberService service = MemberService.getInstance();
+			
+			boolean b = service.ad_MemberUpdate(id, pw, name, email, phone);
+			
+			if(b == true) {
+			
+		
+				
+				resp.sendRedirect("MemberControl?command=ad_admin&id="+id);
+			}else {
+		
+				resp.sendRedirect("MemberControl?command=ad_admin&id="+id);
+			}
+			
+		}
+	
+		
+		
+		else if(command.equals("profileeditaf")) {
+			String id = req.getParameter("id");
+			MemberService service = MemberService.getInstance();
+			
+			MemberDto dto = new MemberDto(id, null, null, null, null, 0, 3);
+			
+			
+			req.setAttribute("dto", dto);
+		
+			dispatch("profileeditaf.jsp", req, resp);
+		}
+		
+		
+		else if(command.equals("mypage")) {
+			String id = req.getParameter("id");
+			
+			MemberService service = MemberService.getInstance();
+			ReserveService ser = ReserveService.getInstance();
+			HotelService Hser = HotelService.getInstance();
+			
+			String hotelname= service.getHotelname(id);
+			
+			MemberDto dto = service.login(id);
+
+			List<ReserveDto> reserveList = service.reserveList(id); 
+
+			req.setAttribute("dto", dto);
+			req.setAttribute("reserveList", reserveList);
+			dispatch("mypage.jsp", req, resp);
+			
+			
+		}
+		
+		else if(command.equals("profileedit")) {
+			String id = req.getParameter("id");
+			MemberService service = MemberService.getInstance();
+			
+			MemberDto dto = service.login(id);
+
+		
+			req.setAttribute("dto", dto);
+		
+			dispatch("profileedit.jsp", req, resp);
+		}
+
+		
+		
+		
+		else if(command.equals("profileeditaf")) {
+			String id = req.getParameter("id");
+			MemberService service = MemberService.getInstance();
+			
+			MemberDto dto = service.login(id);
+					
+			req.setAttribute("dto", dto);
+		
+			dispatch("profileeditaf.jsp", req, resp);
+		}
+		
+	
+else if(command.equals("ad_memberDeleteGo")) {
+		
+
+		String findWord = req.getParameter("txt");
+		String choice = req.getParameter("sel");
+		
+
+		if(choice == null || choice.equals("")){
+			choice = "";	
+		}
+
+		if(findWord == null){
+			findWord = "";
+		}
+		
+		
+		MemberService service = MemberService.getInstance();
+		
+		List<MemberDto> list = service.ad_memlist(findWord, choice);
+		
+		
+		req.setAttribute("list", list);
+		
+		dispatch("ad_memberDelete.jsp", req, resp);
+	}
+		
+	else if(command.equals("memberblack")) {
+	String id = req.getParameter("id");
+	
+	MemberService service = MemberService.getInstance();
+
+	boolean b = service.ad_memDel(id);
+	
+	
+	
+	dispatch("MemberControl?command=ad_memberDeleteGo", req, resp);
+
+}
+		
+	}
+
+	
+	
+	
+	
+
+	public void dispatch(String urls, HttpServletRequest req, HttpServletResponse resp)throws ServletException, IOException {
+		RequestDispatcher dispatch = req.getRequestDispatcher(urls);
+		dispatch.forward(req, resp);		
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
